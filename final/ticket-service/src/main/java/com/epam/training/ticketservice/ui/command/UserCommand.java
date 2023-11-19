7c0135ae0696a110c93c6675fa93c1a96529e3de
Kadosa <lelekkadosa@gmail.com>
2023-11-19 12:54:20 +0100

    Admin login was corrected based on the assignment description

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/command/UserCommand.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/command/UserCommand.java
@@ -1,9 +1,14 @@
 package com.epam.training.ticketservice.ui.command;
 
 import com.epam.training.ticketservice.core.user.UserService;
+import com.epam.training.ticketservice.core.user.model.UserDto;
+import com.epam.training.ticketservice.core.user.persistence.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+
+import java.util.Optional;
 
 @ShellComponent
 @RequiredArgsConstructor
@@ -11,25 +16,30 @@ public class UserCommand {
 
     private final UserService userService;
 
-    @ShellMethod(key = "user logout", value = "User logout")
+    @ShellMethod(key = "sign out", value = "User logout")
     public String logout() {
         return userService.logout()
                 .map(userDto -> userDto + " is logged out!")
                 .orElse("You need to login first!");
     }
 
-    @ShellMethod(key = "user login", value = "User login")
+    @ShellMethod(key = "sign in privileged", value = "User login")
     public String login(String username, String password) {
         return userService.login(username, password)
                 .map(userDto -> userDto + " is successfully logged in!")
-                .orElse("No such username or password!");
+                .orElse("Login failed due to incorrect credentials");
     }
 
-    @ShellMethod(key = "user print", value = "Get user information")
+    @ShellMethod(key = "describe account", value = "Get user information")
     public String print() {
+        Optional<UserDto> user = userService.describe();
+        if (!user.isEmpty() && user.get().role() == User.Role.ADMIN) {
+            return "Signed in with privileged account '" + user.get().username()+"'";
+        }
+
         return userService.describe()
                 .map(Record::toString)
-                .orElse("You need to login first!");
+                .orElse("You are not signed in");
     }
 
     @ShellMethod(key = "user register", value = "User registration")
